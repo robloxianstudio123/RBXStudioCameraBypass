025460534449cc45dd699767bfdca47f171665f4
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2020-10-01 12:38:46 +0200

    Improve code quality

--- CameraFix.lua
+++ CameraFix.lua
@@ -1,7 +1,9 @@
 local RunService = game:GetService("RunService")
-local function aaa()
-	if RunService:IsStudio() then -- Checking if it is studio
+
+local function fixCamera()
+	if RunService:IsStudio() then -- Checks if game is being run in studio
 		workspace.CurrentCamera.CameraType = Enum.CameraType.Custom
 	end
 end
-workspace.CurrentCamera:GetPropertyChangedSignal("CameraType"):Connect(aaa)
+
+workspace.CurrentCamera:GetPropertyChangedSignal("CameraType"):Connect(fixCamera)
